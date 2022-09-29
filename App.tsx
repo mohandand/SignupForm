dd977efaed9d52f8fb526459412b05ad0f9caf2a
mohandand <mohandand@gmail.com>
2022-09-28 18:02:11 -0700

    1 file modified, 1 added, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import './style.css';
-import SignUp from "./SignUp.js"
+import SignUp from './SignUp';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <SignUp />
     </div>
   );
 }
--- SignUp.js
+++
@@ -1,9 +0,0 @@
-import React,{useState} from react;
-
-export default function SignUp(){
-  return(
-    <div>
-      <h1>SignUp Form</h1>
-    </div>
-  )
-}
