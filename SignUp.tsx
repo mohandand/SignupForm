7dd655c2b0341050bfb96b478d791e06ad97796c
mohandand <mohandand@gmail.com>
2022-09-28 20:54:35 -0700

    2 files modified

--- SignUp.tsx
+++ SignUp.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import './style.css';
 
 export default function SignUp() {
   return (
-    <div>
+    <div className="container">
       <h1>SignUp Form</h1>
+      <label>FirstName</label>
+      <input type="text" placeholder="First Name..." />
+      <label>LastName</label>
+      <input type="text" placeholder="First Name..." />
     </div>
   );
 }
