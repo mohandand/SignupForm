e4943939fb610355deea5c3ef369e0f7536c2dc7
mohandand <mohandand@gmail.com>
2022-09-29 10:42:12 -0700

    1 file modified

--- SignUp.tsx
+++ SignUp.tsx
@@ -18,12 +18,12 @@ function App() {
     setIsSubmit(true);
   };
 
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formErrors]);
+  // useEffect(() => {
+  //   console.log(formErrors);
+  //   if (Object.keys(formErrors).length === 0 && isSubmit) {
+  //     console.log(formValues);
+  //   }
+  // }, [formErrors]);
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
